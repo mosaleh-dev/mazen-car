7e5d47db90ec5759ecf1e5fbf9983ed8ecbb5560
mosaleh <mosaleh@mail.com>
2025-05-03 02:43:53 +0300

    chore update vite.config to enable html page

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
     target: 'es2022',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        adminDashboard: resolve(__dirname, 'admin-dashboard.html'),
+        adminCars: resolve(__dirname, 'admin-cars.html'),
+        adminBookings: resolve(__dirname, 'admin-bookings.html'),
+        adminReports: resolve(__dirname, 'admin-reports.html'),
+        logIN: resolve(__dirname, 'login.html'),
+        signUp: resolve(__dirname, 'signup.html'),
+      },
+    },
   },
   server: {
     host: true,
